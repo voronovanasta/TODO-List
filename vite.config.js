6c0aeb6b6e1dd9bb9a790d5ffc406dbd565be7fd
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2025-02-03 23:34:00 +0300

    feat: add settings for gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/[REPO_NAME]/',
+  base: '/TODO-List/',
   build: {
     outDir: 'build',
   },
