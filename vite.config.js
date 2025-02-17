9aaa9a0fcb304fc0b5117d1bb8eb88edf15edf7d
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2025-02-17 14:43:36 +0300

    fix: fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './TODO-List/',
   plugins: [react()],
-  base: '/TODO-List',
   build: {
     outDir: 'build',
   },
