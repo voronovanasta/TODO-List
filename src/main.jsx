76374fbb93f79bdb9f5debff2b622559e41e9401
anastasiya voronova <buchneva.anastasya92@gmail.comm>
2025-02-17 15:57:29 +0300

    fix: fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 //import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TODO-List/',
+  //base: '/TODO-List/',
   plugins: [react()],
   build: {
     outDir: 'build',
